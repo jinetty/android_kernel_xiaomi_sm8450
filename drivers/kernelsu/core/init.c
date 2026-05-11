196d7cacd640faa4d0c1550721629131ebc20adb
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2026-05-11 23:13:14 +0800

    kernelsu: selinux_hide: fix attr/current detection (https://github.com/tiann/KernelSU/pull/3459)

--- drivers/kernelsu/core/init.c
+++ drivers/kernelsu/core/init.c
@@ -13,6 +13,7 @@
 #include "manager/manager_observer.h"
 #include "manager/throne_tracker.h"
 #include "hook/syscall_hook_manager.h"
+#include "hook/lsm_hook.h"
 #include "runtime/ksud.h"
 #include "runtime/ksud_boot.h"
 #include "supercall/supercall.h"
@@ -131,6 +132,8 @@ int __init kernelsu_init(void)
 
 	ksu_feature_init();
 
+	ksu_lsm_hook_init();
+
 	ksu_selinux_hide_init();
 
 	ksu_supercalls_init();
@@ -224,6 +227,8 @@ void __exit kernelsu_exit(void)
 #endif
 
 	ksu_selinux_hide_exit();
+
+	ksu_lsm_hook_exit();
 
 	ksu_feature_exit();
 
